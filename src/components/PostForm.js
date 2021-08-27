358d2e16385bdabe8923ad25e7199e58a5ab8f18
yury trypolski <yury1011.89@gmail.com>
2021-08-27 13:30:01 +0300

    installing redux and react-redux also in component PostForm clearing input does not work

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,11 +4,31 @@ export default class PostForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      title: "",
+    };
   }
 
   submitHandler = (e) => {
     e.preventDefault();
+
+    const newPost = {
+      title: this.state.title,
+      id: Date.now().toString(),
+    };
+
+    console.log(newPost);
+
+    this.setState({ title: "" });
+  };
+
+  changeInputHandler = (e) => {
+    this.setState((prev) => ({
+      ...prev,
+      ...{
+        [e.target.name]: e.target.value,
+      },
+    }));
   };
 
   render() {
@@ -16,9 +36,16 @@ export default class PostForm extends React.Component {
       <form onSubmit={this.submitHandler}>
         <div className="form-group">
           <label htmlFor="title">Заголовок поста</label>
-          <input type="text" className="form-control" id="title" />
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            value={this.state.title.value}
+            onChange={this.changeInputHandler}
+          />
         </div>
-        <button className="btn btn-success" type="submit">
+        <button className="btn btn-success mt-2" type="submit">
           Создать
         </button>
       </form>
